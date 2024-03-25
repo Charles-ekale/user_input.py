568cece9f67e4aea7f385abbf8c83dad5428d9e7
charles ekale <99409935+Charles-ekale@users.noreply.github.com>
2024-03-25 10:21:46 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name=str(input("Enter Your Name: "))
+age=int(input("Enter Your Age: "))
+location=str(input("Enter Your Location: "))
+
+print("Hello", name, "you are", age, "years old and live in",location )
